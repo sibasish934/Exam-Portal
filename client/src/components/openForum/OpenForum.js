7fd5fed11fc07d4db7061f23b38fe1cb7d9dcd82
Sibasish Satapathy <78403992+sibasish934@users.noreply.github.com>
2023-03-24 13:07:41 +0530

    Add files via upload

---
+++ client/src/components/openForum/OpenForum.js
@@ -0,0 +1,29 @@
+import React from "react";
+import {useNavigate} from "react-router-dom";
+import "./open.css";
+const OpenForum = () =>{
+    const navigate = useNavigate();
+    const handleClick = ()=>{
+       navigate('/post');
+    }
+    return(
+        <>
+           <div className="heading">
+             <h2> Welcome To OpenForum</h2>
+             <p> Space of conversion between students to discuss and propose ideas and solution to different questions.</p>
+             <button onClick={handleClick}>Post A solution</button>
+           </div>
+
+            <div className="open-forum-content">
+                <h2>Question1</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <div className="socials">
+                <i class="fa-regular fa-heart"></i><span>67</span>
+                <i class="fa-regular fa-comment"></i><span>34</span>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default OpenForum;
