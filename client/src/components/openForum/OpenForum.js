7cec969c71c0b2607ac4e9b8d0bec659bd6dc811
Sibasish Satapathy <78403992+sibasish934@users.noreply.github.com>
2023-04-07 12:06:33 +0530

    Add files via upload

--- client/src/components/openForum/OpenForum.js
+++ client/src/components/openForum/OpenForum.js
@@ -21,6 +21,20 @@ const OpenForum = () =>{
                 <i class="fa-regular fa-heart"></i><span>67</span>
                 <i class="fa-regular fa-comment"></i><span>34</span>
                 </div>
+
+                <h2>Question2</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <div className="socials">
+                <i class="fa-regular fa-heart"></i><span>67</span>
+                <i class="fa-regular fa-comment"></i><span>34</span>
+                </div>
+
+                <h2>Question3</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <div className="socials">
+                <i class="fa-regular fa-heart"></i><span>67</span>
+                <i class="fa-regular fa-comment"></i><span>34</span>
+                </div>
             </div>
         </>
     )
